b4874a0731a6c632bb83cd7f0d63ded2b89bbe81
ASHENEE123 <159702514+ASHENEE123@users.noreply.github.com>
2025-07-25 11:19:21 +0530

    Update colordetectation.py

--- colordetectation.py
+++ colordetectation.py
@@ -1,12 +1,14 @@
 import cv2
 import numpy as np
-img=cv2.imread(r"D:\collage-image-same-happy-young-260nw-1416089468.webp")
-resize=cv2.resize(img,(700,500))
-cvt_color=cv2.cvtColor(resize,cv2.COLOR_BGR2HSV)
-lower=np.array([0,0,0])
-higher=np.array([225,255,225])
-mask=cv2.inRange(cvt_color,lower,higher)
-cv2.imshow("original img",img)
-cv2.imshow("color detected image",mask)
-cv2.waitKey(0)
+img=cv2.VideoCapture(0)
+while img.isOpened:
+    _,res=img.read()
+    color=cv2.cvtColor(res,cv2.COLOR_BGR2RGB)
+    lower=np.array([0,0,0])
+    higher=np.array([120,102,120])
+    mask=cv2.inRange(color,lower,higher)
+    cv2.imshow("original img",mask)
+    if cv2.waitKey(1) & 0XFF==ord('q'):
+         break
+img.release()
 cv2.destroyAllWindows()
